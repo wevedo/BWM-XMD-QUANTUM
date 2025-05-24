40854c092457972662e0da6c81159f77ab0a1df5
wevedo <mribraahtech@gmail.com>
2025-05-24 12:45:45 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+ok remove this updates that you made there in the last code because is make bot to restart all the time just make to get sessions in groups only not i private chat
+
+only get session per group and safe it to use and why is it that bot works for some groups and others not fix that
+
+don't show in log when you receive messages or get session and make only 1 per group
